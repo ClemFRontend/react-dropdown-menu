3f593f642301cc8e76e90bc59b677fcfabe2b8d5
clément perreaut <clement.perreaut@diiage.org>
2023-04-08 11:13:41 +0200

    🚀 Upgrade logic of component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import DropdownMenu from './components/DropdownMenu/DropdownMenu';
-import Element from './components/DropdownMenu/components/Element/Element'
 import ICON_PLANNING from './assets/icon-planning.svg'
 import ICON_REMINDERS from './assets/icon-reminders.svg'
 import ICON_TODO from './assets/icon-todo.svg'
+import { DropdownList, DropdownMenu, Element } from './components/DropdownList/DropdownList';
+
+
 
 // const ICON_PLANNING = <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0Zm0 2.133a5.867 5.867 0 1 0 0 11.734A5.867 5.867 0 0 0 8 2.133ZM8 3.2a4.8 4.8 0 1 1 0 9.6 4.8 4.8 0 0 1 0-9.6Zm-.533 2.667a.533.533 0 0 0-.534.533v2.133c0 .295.24.534.534.534h3.2a.533.533 0 0 0 0-1.067H8V6.4a.533.533 0 0 0-.533-.533Z" fill="#8E4CB6"/></svg>
 // const ICON_REMINDERS = <svg width="13" height="17" xmlns="http://www.w3.org/2000/svg"><path d="M6.408 13.916c-3.313 0-6-1.343-6-3 0-.612.371-1.18 1-1.654V7.916a5 5 0 0 1 3.041-4.6 2 2 0 0 1 3.507-1.664 2 2 0 0 1 .411 1.664 5.002 5.002 0 0 1 3.041 4.6v1.346c.629.474 1 1.042 1 1.654 0 1.657-2.687 3-6 3Zm0 1c.694 0 1.352-.066 1.984-.16.004.054.016.105.016.16a2 2 0 0 1-4 0c0-.055.012-.106.016-.16.633.094 1.29.16 1.984.16Z" fill="#EDD556"/></svg>
@@ -13,18 +14,20 @@ function App() {
   return (
     <div className="App">
       <h1>Dropdown Menu</h1>
-      <ul>
+      <DropdownList>
         <DropdownMenu title="Features">
           <Element icon={<img src={ICON_PLANNING} width={20} height={20} alt='planning-icon' />}>Planning</Element>
           <Element icon={<img src={ICON_REMINDERS} width={16} height={20} alt='planning-icon' />}>Reminders</Element>
           <Element icon={<img src={ICON_TODO} width={20} height={20} alt='planning-icon' />}>To Do</Element>
-          <DropdownMenu title="Others">
+          <DropdownMenu title="Sub dropmenu">
             <Element>Item</Element>
             <Element>Item</Element>
             <Element>Item</Element>
           </DropdownMenu>
         </DropdownMenu>
-      </ul>
+        <Element>Item</Element>
+        <Element>Item</Element>
+      </DropdownList>
 
       {/* <DropdownMenu title="First menu">
         <p>🚀 First item</p>
--- src/components/DropdownList/DropdownList.jsx
+++ src/components/DropdownList/DropdownList.jsx
@@ -1,10 +1,30 @@
 import React, { useEffect, useState } from 'react'
-import './DropdownMenu.css'
+import './DropdownList.css'
 
 import ICON_ARROW_DOWN from '../../assets/icon-arrow-down.svg'
-import ICON_ARROW_UP from '../../assets/icon-arrow-up.svg'
+// import ICON_ARROW_UP from '../../assets/icon-arrow-up.svg'
 
-function DropdownMenu({title, icon, children}) {
+/**
+ * 
+ * Englobe dropdown menus
+ * 
+ * @param {any} children
+ * @returns {HTMLElement}
+ */
+export function DropdownList({children}) {
+  return <ul>{children}</ul>
+}
+
+/**
+ * 
+ * Everything under this tag will be collapsable
+ * 
+ * @param {String} title 
+ * @param {HTMLElement} icon
+ * @param {String} children
+ * @returns {HTMLElement}
+ */
+export function DropdownMenu({title, icon, children}) {
 
     const [display, setDisplay] = useState('none')
     const [rotation, setRotation] = useState('0')
@@ -64,20 +84,26 @@ function DropdownMenu({title, icon, children}) {
                 {children}
             </ul>
         </li>
-        // <div className='dropdown-menu'>
-        //     <div className='dropdown-title-container' onClick={() => toogleDisplay()}>
-        //         <p>{title}</p>
-        //         <img 
-        //             style={{rotate: rotation}}
-        //             src={ICON_ARROW_UP} alt='arrow' 
-        //             width={10}
-        //             height={6} />
-        //     </div>
-        //     <div className='dropdown-items' style={{display: display}}>
-        //         {children}
-        //     </div>
-        // </div>
     )
 }
 
-export default DropdownMenu
+/**
+ * 
+ * Element of list. Can be under DropdownMenu or DropdownList.
+ * 
+ * @param {HTMLElement} icon
+ * @param {String} children
+ * @returns 
+ */
+export function Element({icon, children}) {
+  return (
+      <li className='element'>
+          {icon !== undefined && 
+              <div className='element-icon'>
+                  {icon}
+              </div>
+          }
+          <p>{children}</p>
+      </li>
+  )
+}
